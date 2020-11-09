b84c5b3b0587f729cfb2b2abb13ab28c108bbaca
Fernando jose Mejia <>
2020-11-09 18:45:57 -0400

    comit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './products/services/services.component';
+import { CartComponent } from './products/cart/cart.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+  { path: '', component: HomeComponent},
+  { path: 'user', component: UserComponent},
+  {path: 'signin', component: SignInComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'products', component: ProductsComponent},
+  { path: 'cart', component: CartComponent },
+  { path: 'admin', component: AdminComponent},
+  { path: 'admin-products', component: AdminProductsComponent },
+  { path: 'users', component: AdminUsersComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,53 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './products/services/services.component';
+import { CartComponent } from './products/cart/cart.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { HomeComponent } from './home/home.component';
+var firebaseConfig = {
+      apiKey: "AIzaSyBEo-AJ8Kfw9iyi4PkRWB6bMwcpeikA_cw",
+      authDomain: "candylandapp-f4eac.firebaseapp.com",
+      databaseURL: "https://candylandapp-f4eac.firebaseio.com",
+      projectId: "candylandapp-f4eac",
+      storageBucket: "candylandapp-f4eac.appspot.com",
+      messagingSenderId: "576862896650",
+      appId: "1:576862896650:web:dc76616aea8adfd9edd5f3",
+      measurementId: "G-E4BBMCH6HH"
+    };
+  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    AdminProductsComponent,
+    AdminUsersComponent,
+    ProductsComponent,
+    ServicesComponent,
+    CartComponent,
+    UserComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent
   ],
   imports: [
+   
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
